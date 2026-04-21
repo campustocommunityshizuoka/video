6a8fdc36a0c89b78a59211992be195fd5821eab6
rai <campustocommunityshizuoka@gmail.com>
2026-04-21 20:16:00 +0900

    修正2

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 //@ts-ignore
-import "../app/globals.css";
+import "../globals.css";
 import { getDictionary } from '@/utils/get-dictionary';
 
 const geistSans = Geist({
